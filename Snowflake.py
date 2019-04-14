81f0730001af65f8d541a47e7548ed69dc95e591
GuyBoaron <the.guybo.man@gmail.com>
2019-04-14 11:59:49 +0300

    a

--- Snowflake.py
+++ Snowflake.py
@@ -1,3 +1,10 @@
+from subprocess import Popen, PIPE
+
+sudo_password = 'ncjwwr400'
+command = 'sudo apt-get install python3-tk'.split()
+
+p = Popen(['sudo', '-S'] + command, stdin=PIPE, stderr=PIPE, universal_newlines=True)
+sudo_prompt = p.communicate(sudo_password + '\n')[1]
 
 import os
 from turtle import Turtle
